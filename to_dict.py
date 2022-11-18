b94020336d04ade2616f7819470f2cb5baefae95
oshamashama <65126083+oshamashama@users.noreply.github.com>
2022-11-18 13:56:03 +0900

    convert csv -> cpp vector

---
+++ to_dict.py
@@ -0,0 +1,17 @@
+import csv
+
+with open('best_margin.csv', 'r') as f:
+  data = csv.reader(f)
+  v = [[] for _ in range(101)]
+  for l in data:
+    v[int(l[0])].append(int(l[2]))
+  with open('vec_map', 'w') as f2:
+    f2.write('{')
+    for l in v:
+      f2.write('{')
+      for llv in l:
+        memo =  str(llv) + ','
+        f2.write(memo)
+      f2.write('0},\n')
+    f2.write('{0}}')
+      
